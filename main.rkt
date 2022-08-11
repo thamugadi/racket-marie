2d53b8574e682982a66cce451a709c27a63e63e0
aramya <mdnh@protonmail.com>
2022-08-11 04:33:26 +0100

    Update main.rkt

--- main.rkt
+++ main.rkt
@@ -1,9 +1,11 @@
 #lang racket
+
 (define-syntax define-instruction
   (syntax-rules () ((_ instr opcode)
                     (define-syntax instr
                       (syntax-rules () ((_ arg)
                        (bitwise-ior opcode (bitwise-and #xfff (bitwise-ior opcode arg)))))))))
+                       
 (define (ac m) (car m)) (define (ir m) (cadr m)) (define (pc m) (caddr m))
 (define (write-ac m x) (append (list x) (cdr m)))
 (define (write-ir m x) (append (take m 1) (list x) (cddr m)))
@@ -25,18 +27,13 @@
      ((== #x2000) (write-mem m  (op-arg m) (ac m)))
      ((== #x3000) (write-ac  m  (+ (ac m) (access-mem m (op-arg m)))))
      ((== #x4000) (write-ac  m  (- (ac m) (access-mem m (op-arg m)))))
-     ((== #x5000) (write-ac  m  (read)))
-     ((== #x6000) (print (ac m)))
-     ((== #x7000) (exit))
+     ((== #x5000) (write-ac  m  (read))) ((== #x6000) (print (ac m)))
+     ((== #x7000) (exit)) 
      ((== #x8000) (if (= (ac m) 0) (write-pc m (+ (pc m) 1)) m))
      ((== #x9000) (write-pc m (op-arg m)))))
 
 (define (init-machine memsize) (list 0 0 0 (map (lambda (x) (* x 0)) (range memsize))))
 
-
-(define-instruction load     #x1000) (define-instruction store    #x2000)
-(define-instruction add      #x3000) (define-instruction sub      #x4000)
-(define-instruction input    #x5000) (define-instruction output   #x6000)
-(define-instruction halt     #x7000) (define-instruction skipcond #x8000)
-(define-instruction jump     #x9000)
-
+(define-instruction load     #x1000) (define-instruction store    #x2000) (define-instruction add      #x3000) 
+(define-instruction sub      #x4000) (define-instruction input    #x5000) (define-instruction output   #x6000)
+(define-instruction halt     #x7000) (define-instruction skipcond #x8000) (define-instruction jump     #x9000)
